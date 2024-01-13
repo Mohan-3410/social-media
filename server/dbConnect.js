19043320d617ff2d7df349e33a307c646f2c6bf2
mohan_2231 <mahan.singh.tomar1034@gmail.com>
2024-01-13 15:29:03 +0530

    add mongoose userId and password to mongoose

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
-    const url = "mongodb+srv://mohan1034:lzFqEOnYyyHiFJh3@cluster0.3r80h15.mongodb.net/socialMedia?retryWrites=true&w=majority";
+    const url = `mongodb+srv://${process.env.MONGOOSE_ID}:${process.env.MONGOOSE_PASSWORD}@cluster0.3r80h15.mongodb.net/socialMedia?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(url)
         console.log(`mongodb connected ${mongoose.connection.host} : ${mongoose.connection.port}`)
@@ -9,5 +9,5 @@ module.exports = async () => {
         console.log("connection error", error.message)
         process.exit(1);
     }
-    
+
 }
